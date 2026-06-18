e826e535af4a5e58c62fca8107fc2862200b78c1
Kyle Swindell <kyle@parasolutions.com>
2026-06-18 09:05:08 -0400

    reference: add carbon source snapshot

---
+++ reference/carbon-main/packages/react/src/components/ExpandableSearch/ExpandableSearch.tsx
@@ -0,0 +1,97 @@
+/**
+ * Copyright IBM Corp. 2021, 2026
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React, { useState, useRef, useEffect, forwardRef } from 'react';
+import classnames from 'classnames';
+import Search, { type SearchProps } from '../Search';
+import { usePrefix } from '../../internal/usePrefix';
+import { composeEventHandlers } from '../../tools/events';
+import { match, keys } from '../../internal/keyboard';
+import { mergeRefs } from '../../tools/mergeRefs';
+import { isSearchValuePresent } from '../Search/utils';
+
+const frFn = forwardRef<HTMLInputElement, SearchProps>;
+
+const ExpandableSearch = frFn((props, forwardedRef) => {
+  const {
+    onBlur,
+    onChange,
+    onExpand,
+    onKeyDown,
+    defaultValue,
+    isExpanded,
+    ...rest
+  } = props;
+
+  const [expanded, setExpanded] = useState(isExpanded || false);
+  const [hasContent, setHasContent] = useState(
+    isSearchValuePresent(defaultValue)
+  );
+  const searchRef = useRef<HTMLInputElement>(null);
+  const prefix = usePrefix();
+
+  function handleBlur(evt) {
+    const relatedTargetIsAllowed =
+      evt.relatedTarget &&
+      evt.relatedTarget.classList.contains(`${prefix}--search-close`);
+
+    if (expanded && !relatedTargetIsAllowed && !hasContent && !isExpanded) {
+      setExpanded(false);
+    }
+  }
+
+  useEffect(() => {
+    setExpanded(!!isExpanded);
+  }, [isExpanded]);
+
+  function handleChange(evt) {
+    setHasContent(evt.target.value !== '');
+  }
+
+  function handleExpand() {
+    setExpanded(true);
+    searchRef.current?.focus?.();
+  }
+
+  function handleKeyDown(evt) {
+    if (expanded && match(evt, keys.Escape)) {
+      evt.stopPropagation();
+
+      // escape key only clears if the input is empty, otherwise it clears the input
+      if (!evt.target?.value && !isExpanded) {
+        setExpanded(false);
+      }
+    }
+  }
+
+  const classes = classnames(
+    `${prefix}--search--expandable`,
+    {
+      [`${prefix}--search--expanded`]: expanded,
+    },
+    rest.className
+  );
+
+  return (
+    <Search
+      {...rest}
+      defaultValue={defaultValue}
+      isExpanded={expanded}
+      ref={mergeRefs(searchRef, forwardedRef)}
+      className={classes}
+      onBlur={composeEventHandlers([onBlur, handleBlur])}
+      onChange={composeEventHandlers([onChange, handleChange])}
+      onExpand={composeEventHandlers([onExpand, handleExpand])}
+      onKeyDown={composeEventHandlers([onKeyDown, handleKeyDown])}
+    />
+  );
+});
+
+ExpandableSearch.propTypes = Search.propTypes;
+ExpandableSearch.displayName = 'ExpandableSearch';
+
+export default ExpandableSearch;
---
+++ reference/carbon-main/packages/utilities/src/carousel/carousel-test.js
@@ -0,0 +1,232 @@
+/**
+ * Copyright IBM Corp. 2025, 2026
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { initCarousel } from './carousel';
+
+describe('initCarousel', () => {
+  let container;
+  let mockOnViewChangeStart;
+  let mockOnViewChangeEnd;
+
+  beforeEach(() => {
+    document.body.innerHTML = `
+    <div id="carousel">
+      <div>Slide 1</div>
+      <div>Slide 2</div>
+      <div>Slide 3</div>
+    </div>
+  `;
+
+    container = document.getElementById('carousel');
+    mockOnViewChangeStart = jest.fn();
+    mockOnViewChangeEnd = jest.fn();
+  });
+
+  test('initializes carousel with correct classes and wrapper', () => {
+    initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    const wrapper = container.querySelector('.carousel__itemsWrapper');
+    expect(wrapper).toBeInTheDocument();
+    expect(container.classList.contains('carousel__view-stack')).toBe(true);
+    expect(wrapper?.children.length).toBe(3);
+  });
+
+  test('initializes carousel with correct a11y attributes', () => {
+    initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    const liveRegion = container.querySelector('.carousel__live-region');
+    expect(liveRegion).toBeInTheDocument();
+    expect(liveRegion.getAttribute('aria-live')).toBe('polite');
+    expect(liveRegion.getAttribute('aria-atomic')).toBe('true');
+    expect(liveRegion.textContent).toBe('Item 1 of 3');
+
+    const wrapper = container.querySelector('.carousel__itemsWrapper');
+    const children = [...wrapper.children];
+    children.forEach((child, idx) => {
+      if (idx === 0) {
+        expect(child.getAttribute('aria-hidden')).toBe(null);
+        expect(child.getAttribute('inert')).toBe(null);
+      } else {
+        expect(child.getAttribute('aria-hidden')).toBe('true');
+        expect(child.getAttribute('inert')).toBe('');
+      }
+    });
+  });
+
+  test('only generates live region once', () => {
+    initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    expect(container.querySelectorAll('.carousel__live-region').length).toBe(1);
+
+    initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    expect(container.querySelectorAll('.carousel__live-region').length).toBe(1);
+  });
+
+  test('getActiveItem returns correct index and item', () => {
+    const carousel = initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    const active = carousel.getActiveItem();
+    expect(active.index).toBe(0);
+    expect(active.item?.textContent).toBe('Slide 1');
+  });
+
+  test('navigate next triggers onViewChangeStart and onViewChangeEnd', () => {
+    const carousel = initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    carousel.next();
+    const active = carousel.getActiveItem();
+
+    expect(mockOnViewChangeStart).toHaveBeenCalled();
+    expect(mockOnViewChangeEnd).toHaveBeenCalled();
+    expect(active.index).toBe(1);
+  });
+
+  test('should navigate to the previous view', () => {
+    const carousel = initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+    expect(container.querySelector('.carousel__live-region').textContent).toBe(
+      'Item 1 of 3'
+    );
+    carousel.next(); // go to index 1
+    expect(container.querySelector('.carousel__live-region').textContent).toBe(
+      'Item 2 of 3'
+    );
+    carousel.prev();
+    expect(container.querySelector('.carousel__live-region').textContent).toBe(
+      'Item 1 of 3'
+    );
+    const active = carousel.getActiveItem();
+    expect(active.index).toBe(0);
+  });
+
+  test('nav controls handle out of bounds', () => {
+    const carousel = initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    // attempt to go below first index
+    carousel.prev();
+    expect(container.querySelector('.carousel__live-region').textContent).toBe(
+      'Item 1 of 3'
+    );
+    expect(carousel.getActiveItem().index).toBe(0);
+
+    // attempt to go beyond the last index
+    for (let i = 0; i <= 3; i++) {
+      carousel.next();
+    }
+    expect(container.querySelector('.carousel__live-region').textContent).toBe(
+      'Item 3 of 3'
+    );
+    expect(carousel.getActiveItem().index).toBe(2);
+  });
+
+  test('goToIndex navigates to correct slide', () => {
+    const carousel = initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    carousel.goToIndex(2);
+    const active = carousel.getActiveItem();
+
+    expect(active.index).toBe(2);
+    expect(active.item?.textContent).toBe('Slide 3');
+  });
+
+  test('goToIndex handles out of bounds', () => {
+    const carousel = initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    let active;
+
+    carousel.goToIndex(-1);
+    active = carousel.getActiveItem();
+    expect(active.index).toBe(0);
+    expect(active.item?.textContent).toBe('Slide 1');
+    expect(container.querySelector('.carousel__live-region').textContent).toBe(
+      'Item 1 of 3'
+    );
+
+    carousel.goToIndex(999999);
+    active = carousel.getActiveItem();
+    expect(active.index).toBe(2);
+    expect(active.item?.textContent).toBe('Slide 3');
+    expect(container.querySelector('.carousel__live-region').textContent).toBe(
+      'Item 3 of 3'
+    );
+  });
+
+  test('reset goes back to first slide', () => {
+    const carousel = initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    carousel.goToIndex(2);
+    carousel.reset();
+    const active = carousel.getActiveItem();
+
+    expect(active.index).toBe(0);
+  });
+
+  test('should stop transition callbacks after `destroyEvents`', () => {
+    const carousel = initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    mockOnViewChangeEnd.mockClear();
+
+    const activeItem = carousel.getActiveItem().item;
+
+    activeItem?.dispatchEvent(new Event('transitionend'));
+    expect(mockOnViewChangeEnd).toHaveBeenCalledTimes(1);
+
+    carousel.destroyEvents();
+    activeItem?.dispatchEvent(new Event('transitionend'));
+    expect(mockOnViewChangeEnd).toHaveBeenCalledTimes(1);
+  });
+
+  test('live region is not created when carousel has no children ', () => {
+    document.body.innerHTML = `<div id="carousel"></div>`;
+    const container = document.getElementById('carousel');
+
+    initCarousel(container, {
+      onViewChangeStart: mockOnViewChangeStart,
+      onViewChangeEnd: mockOnViewChangeEnd,
+    });
+
+    expect(
+      container.querySelector('.carousel__live-region')
+    ).not.toBeInTheDocument();
+  });
+});
